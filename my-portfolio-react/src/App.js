7e3fc47ef144bd2b40900b18aa9941d23446581e
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-08-11 00:12:30 +0100

    Initial commit

---
+++ my-portfolio-react/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi! My name is Francisca Agyekum Poku.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
